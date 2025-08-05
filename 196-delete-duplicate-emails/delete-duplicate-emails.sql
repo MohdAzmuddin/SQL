131e8a5629809acdb32744231402150e25baf683
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-05 21:44:53 +0530

    Update delete-duplicate-emails.sql

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -4,3 +4,12 @@ from Person p1
 join Person p2
 on p1.email = p2.email
  AND p1.id > p2.id;
+
+use self-join the table:
+p1 is the row to be deleted.
+p2 is the row with the same email but smaller id (which we want to keep).
+p1.id > p2.id ensures you only delete duplicates after the first occurrence.
+
+
+
+
