2c34ed9bb0b25546d71fb4d72a80abca17bc4b47
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-07 16:09:02 +0530

    Update friend-requests-ii-who-has-the-most-friends.sql

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -9,3 +9,15 @@ from
 group by id
 order by count(id) desc
 limit 1
+
+or
+    
+with AllIds as (
+    select requester_id as id from RequestAccepted
+    union all 
+    select accepter_id as id from RequestAccepted
+)
+select id, count(*) as num from AllIds
+group by id
+order by num desc
+limit 1;
