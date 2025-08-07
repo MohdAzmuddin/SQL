cab15db9979245315b10731666dc0cd5013ea2a3
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-07 19:13:04 +0530

    Update find-users-with-valid-e-mails.sql

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -2,4 +2,8 @@ select user_id,name, mail
  from Users
 where REGEXP_LIKE(mail,'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$','c');
 
-            
+or
+select user_id, name, mail
+from Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$';
+           
