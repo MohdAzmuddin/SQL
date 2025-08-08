a5228831f2f7413c5392b0981389e52f432f8d1e
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-08 12:02:35 +0530

    Update customers-who-never-order.sql

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -2,3 +2,10 @@
 select name as Customers
 from Customers
 where id not in (select customerId from Orders)
+
+or
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;
