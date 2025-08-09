1a764e62840061dad5bcb4a74282339c826e1435
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-09 17:13:14 +0530

    Update swap-salary.sql

--- 627-swap-salary/swap-salary.sql
+++ 627-swap-salary/swap-salary.sql
@@ -4,3 +4,8 @@ set sex = case
           when sex = 'm' then 'f'
           else 'm'
           end;
+
+or
+# Write your MySQL query statement below
+UPDATE Salary
+SET sex = IF(sex = 'f', 'm', 'f');
