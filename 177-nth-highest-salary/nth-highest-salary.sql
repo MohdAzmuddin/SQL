72af8c1d39d993d0fddb18d5000ac83b492c2cf7
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-09 15:24:15 +0530

    Update nth-highest-salary.sql

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -11,11 +11,18 @@ BEGIN
       ) as rankTable
       where ranked=n
       limit 1
+  );
+END
 
-      #or
-     # select distinct salary 
-      #from Employee
-      #order by salary desc
-      #limit 1 offset N-1
+or
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+      LIMIT 1 OFFSET N
   );
 END
