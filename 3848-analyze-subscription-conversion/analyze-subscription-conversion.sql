ea005647394f77aa238f796fc9fc32f3674dc8e0
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-08-25 23:15:26 +0530

    Update analyze-subscription-conversion.sql

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -18,3 +18,20 @@ FROM trial_avg t
 JOIN paid_avg p 
   ON t.user_id = p.user_id
 ORDER BY t.user_id;
+
+
+OR
+# Write your MySQL query statement below
+
+SELECT *
+FROM (
+    SELECT
+        user_id
+        , ROUND(AVG(CASE WHEN activity_type = 'free_trial' THEN activity_duration END), 2) AS trial_avg_duration
+        , ROUND(AVG(CASE WHEN activity_type = 'Paid' THEN activity_duration END), 2) AS paid_avg_duration
+    FROM useractivity
+    GROUP BY user_id 
+) AS temp
+WHERE paid_avg_duration IS NOT NULL
+
+
