1702430a75f648414ebb0d700464c140f50b3ba8
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2025-09-11 21:16:30 +0530

    Delete 231-power-of-two directory

--- 231-power-of-two/power-of-two.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;   // negative numbers & 0 are not powers of two
-        return (n & (n - 1)) == 0;
-    }
-}
